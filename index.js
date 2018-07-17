e08f5d2c8d122e5031eb37e731a1a5d3a895a4dd
babi <21229455@qq.com>
2018-07-17 18:18:16 +0800

    测试

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const hexoFs = require('hexo-fs');
-const fs = require('fs');
-const path = require('path');
-const ejs = require('ejs');
-const uuidv1 = require('uuid/v1');
-const version = uuidv1();
+const hexoFs = require('hexo-fs')
+const fs = require('fs')
+const path = require('path')
+const ejs = require('ejs')
+const uuidv1 = require('uuid/v1')
+const version = uuidv1()
+
+const distPath = path.join(__dirname, '../../public/')
 
 hexo.on('exit', () => {
-    const distPath = path.join(__dirname, '../../public/');
-    try {
-        let files = hexoFs.listDirSync(distPath, {ignoreHidden: true});
-        ejs.renderFile(path.join(__dirname, './sw.js'), {cacheList: files, version: version || 1}, (err, str) => {
-            if (err) {
-                console.log(err);
-            } else {
-                hexoFs.writeFileSync(path.join(distPath, './sw.js'), str);
-            }
-        });
-    } catch (err) {
-        console.error(err.message);
+  let data = {
+    version
+  }
+  try {
+    let files = hexoFs.listDirSync(distPath, { ignoreHidden: true })
+    data.files = JSON.stringify(files)
+  } catch (err) {
+    console.error(err.message)
+    throw Error('读取文件列表失败')
+  }
+  ejs.renderFile(path.join(__dirname, './sw.ejs'), data, (err, str) => {
+    if (err) {
+      console.log(err)
+    } else {
+      hexoFs.writeFileSync(path.join(distPath, './sw.js'), str)
     }
+  })
 })
